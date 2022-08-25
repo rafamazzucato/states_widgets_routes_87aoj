2c8d6177c5ef6afbfd16f316c4e340d2cda557ee
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-08-24 23:02:33 -0300

    aula 01

---
+++ lib/screens/home.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:states_widgets_routes/models/language.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<Language> languages = [
+    Language("Android Nativo", "Linguagens C, Java, Kotlin"),
+    Language("Ios Nativo", "Linguagens Objective-C, Swift"),
+    Language("Flutter", "Linguagens Dart"),
+    Language("React Native", "Linguagens Javascript e Typescript"),
+    Language("Ionic", "Linguagens Javascript e Typescript")
+  ];
+
+  Widget title = const Text("Minhas Linguagens");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: title),
+      body: Column(
+        children: [
+          Wrap(
+            spacing: 10,
+            children: buildChoices(),
+          ),
+          Expanded(child: ListView(children: buildItemsList()))
+        ],
+      ),
+    );
+  }
+
+  List<ChoiceChip> buildChoices(){
+    return languages.map((l) => ChoiceChip(
+        label: Text(l.title), 
+        selected: l.select,
+        onSelected: (value) => setState(() {
+          l.select = value;
+        }))).toList();
+  }
+
+  List<Widget> buildItemsList(){
+      return languages
+        .where((l) => l.select)
+        .map((l) => Card(
+          child: ListTile(
+            leading: Icon(l.icon),
+            title: Text(l.title),
+            subtitle: Text(l.subTitle),
+          ),
+        )).toList();
+  }
+}
